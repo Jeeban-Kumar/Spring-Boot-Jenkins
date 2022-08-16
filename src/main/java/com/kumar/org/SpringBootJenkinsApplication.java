1930f5a89d4169bb23d1d2938e1a904680ffd2b1
Jeeban-Kumar <jkdashyt2019@gmail.com>
2022-08-16 23:42:11 +0530

    2nd msg

--- src/main/java/com/kumar/org/SpringBootJenkinsApplication.java
+++ src/main/java/com/kumar/org/SpringBootJenkinsApplication.java
@@ -1,12 +1,17 @@
 package com.kumar.org;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootJenkinsApplication {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+	
 	public static void main(String[] args) {
+		logger.info("good msg");
 		SpringApplication.run(SpringBootJenkinsApplication.class, args);
 	}
 
