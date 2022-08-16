2dd27ff3d6469f8f779f8cb84a3da35692b366f0
Jeeban-Kumar <jkdashyt2019@gmail.com>
2022-08-17 00:04:54 +0530

    6th commit

--- src/main/java/com/kumar/org/SpringBootJenkinsApplication.java
+++ src/main/java/com/kumar/org/SpringBootJenkinsApplication.java
@@ -14,7 +14,7 @@ public class SpringBootJenkinsApplication {
 		logger.info("good msg");
 		SpringApplication.run(SpringBootJenkinsApplication.class, args);
 		logger.info("good morning");
-			logger.info("good night");
+		logger.info("good night");
 	}
 
 }
